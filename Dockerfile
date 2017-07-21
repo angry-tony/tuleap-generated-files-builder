00b8048f3080d6b40d355d682568122f60cc9f7d
Thomas Gerbet <thomas.gerbet@enalean.com>
2017-07-21 11:20:51 +0200

    The generation is now done the exact the same way it is done when building the packages

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 FROM ubuntu:16.04
 
 RUN apt-get update -y && \
-    apt-get install -y nodejs npm php ruby-sass gosu expect && \
+    apt-get install -y nodejs npm php ruby-sass git cpio gettext gosu expect && \
     rm -rf /var/lib/apt/lists/* && \
     gem install scss_lint && \
     ln -s /usr/bin/nodejs /usr/bin/node && \
-    npm install --global npm@4 && \
+    npm install --global npm@4 bower gulp-cli && \
+    npm install --no-save phantomjs-prebuilt && \
+    mv /node_modules/phantomjs-prebuilt /usr/local/lib/node_modules/phantomjs-prebuilt && \
+    ln -s /usr/local/lib/node_modules/phantomjs-prebuilt/bin/phantomjs /usr/local/bin/phantomjs && \
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && \
     php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Composer Installer verified'; } else { echo 'Composer Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" && \
     php composer-setup.php && \
--- run-as-owner.sh
+++ run-as-owner.sh
@@ -3,28 +3,27 @@
 set -e
 
 TULEAP_BUILD_TMP_FOLDER='/tmp/tuleap_build'
-TULEAP_BUILD_NPM_FOLDER="$TULEAP_BUILD_TMP_FOLDER/npm"
 
 create_tuleap_build_folders() {
-    mkdir -p "$TULEAP_BUILD_NPM_FOLDER"
+    mkdir -p "$TULEAP_BUILD_TMP_FOLDER"
 }
 
-build_files_generated_through_npm() {
+build_generated_files() {
     # Set the HOME environnement variable is crappy but it seems that is the
     # only way to prevent npm and node-gyp to put their files everywhere
-    TMP="$TULEAP_BUILD_NPM_FOLDER" HOME="$TULEAP_BUILD_NPM_FOLDER" npm install
-    TMP="$TULEAP_BUILD_NPM_FOLDER" HOME="$TULEAP_BUILD_NPM_FOLDER" npm run build
+    TMP="$TULEAP_BUILD_TMP_FOLDER" HOME="$TULEAP_BUILD_TMP_FOLDER" OS='rhel6' make -C "$(pwd)/tools/rpm" tarball
+    rm -rf "$TULEAP_BUILD_TMP_FOLDER"/*
 }
 
 configure_npm_registry(){
     if [ ! -z "$NPM_REGISTRY" ]; then
-        HOME="$TULEAP_BUILD_NPM_FOLDER" npm config set registry "$NPM_REGISTRY"
+        HOME="$TULEAP_BUILD_TMP_FOLDER" npm config set registry "$NPM_REGISTRY"
     fi
     if [ ! -z "$NPM_USER" -a ! -z "$NPM_PASSWORD" -a ! -z "$NPM_EMAIL" ]; then
-      HOME="$TULEAP_BUILD_NPM_FOLDER" /npm-login.sh "$NPM_USER" "$NPM_PASSWORD" "$NPM_EMAIL"
+      HOME="$TULEAP_BUILD_TMP_FOLDER" /npm-login.sh "$NPM_USER" "$NPM_PASSWORD" "$NPM_EMAIL"
     fi
 }
 
 create_tuleap_build_folders
 configure_npm_registry
-build_files_generated_through_npm
+build_generated_files
