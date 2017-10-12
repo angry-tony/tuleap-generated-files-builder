aa5ad8286da922ddf5190f516b0881f90294fb13
Thomas Gerbet <thomas.gerbet@enalean.com>
2017-10-12 16:35:43 +0200

    The container can be used even if we do not mount the output volume
    No more cp: cannot create regular file '/output/tuleap-X.Y.tar.gz': Permission denied

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ get_user_group_id_tuleap_volume() {
     stat -c '%u:%g' /tuleap
 }
 
+chown "$(get_user_group_id_tuleap_volume)" /output
+
 gosu "$(get_user_group_id_tuleap_volume)" "/run-as-owner.sh"
